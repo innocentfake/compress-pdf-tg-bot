ee459af0f2e233cf58787fe5e3bd00cc7af4d365
Nadan parinda <sukalalthakare19@gmail.com>
2025-03-23 23:27:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,29 +3,25 @@ from .config import *
 from pyrogram.enums import ParseMode
 
 class Bot(Client):
-    def __init__(self):
-        super().__init__(
-            "compress-pdf-tg-bot",
-            bot_token=Config.TG_BOT_TOKEN
-            api_id=Config.APP_ID
-            api_hash=Config.API_HASH)
-            plugins={
-                "root": "plugins"
-            }
-        self.LOGGER = LOGGER
+    def __init__(self):
+        super().__init__(
+            "compress-pdf-tg-bot",
+            bot_token=Config.TG_BOT_TOKEN,
+            api_id=Config.APP_ID,
+            api_hash=Config.API_HASH,
+            plugins={"root": "plugins"}
+        )
+        self.LOGGER = LOGGER
 
-    async def start(self):
-        await super().start()
-        me = await self.get_me()
-        self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(
-            f"@{me.username}  started! "
-        )
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        self.set_parse_mode(ParseMode.HTML)
+        self.LOGGER.info(f"@{me.username} started!")
 
-    async def stop(self, *args):
-        await super().stop()
-        self.LOGGER(__name__).info("Bot stopped. Bye.")
-
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER.info("Bot stopped. Bye.")
 
 app = Bot()
 app.run()
