859036dd104ed19b305953489b3a0cae48020a14
Nadan parinda <sukalalthakare19@gmail.com>
2025-03-23 23:06:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,9 +10,9 @@ class Bot(Client):
     def __init__(self):
         super().__init__(
             "compress-pdf-tg-bot",
-            bot_token=Config.py TG_BOT_TOKEN,
-            api_id=Config.py APP_ID,
-            api_hash=Config.py API_HASH,)
+            bot_token=Config.py TG_BOT_TOKEN
+            api_id=Config.py APP_ID
+            api_hash=Config.py API_HASH)
             plugins={
                 "root": "plugins"
             }
